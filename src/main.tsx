d13703243572843ff6e6a9c77b9d64414ad8618e
ryosuke-sato <ryosuke310.engineer@gmail.com>
2024-09-16 16:29:37 +0900

    feat: #2 リセットCSS追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { App } from "./app";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 );
