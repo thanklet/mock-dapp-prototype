85d4d9abd34fcd9484baa615a02e5101643a1b5d
ryosuke-sato <ryosuke310.engineer@gmail.com>
2024-09-24 23:09:31 +0900

    fix: #8 バックナビゲーションに変更

--- src/app/routes/app/thanks/location.tsx
+++ src/app/routes/app/thanks/location.tsx
@@ -1,13 +1,11 @@
 import { ThanksLayout } from "@/components/layouts/thanks-layout";
 import { Head } from "@/components/seo/head";
 import { Location } from "@/features/app/thanks/location/components/location";
-import { useParams } from "react-router-dom";
 export const LocationRoute = () => {
-  const { userId } = useParams();
   return (
     <>
       <Head title={"ロケーション"} />
-      <ThanksLayout backTo={`/app/${userId}/dashboard`} label="ロケーション">
+      <ThanksLayout label="ロケーション">
         <Location />
       </ThanksLayout>
     </>
