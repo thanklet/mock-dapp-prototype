cfda69e8f55ea22ad224193f5776cb32246eaa52
ryosuke-sato <ryosuke310.engineer@gmail.com>
2024-09-26 00:07:40 +0900

    fix: #9 import修正

--- src/app/routes/app/thanks/send.tsx
+++ src/app/routes/app/thanks/send.tsx
@@ -1,13 +1,12 @@
-import { ThanksLayout } from "@/components/layouts/thanks-layout";
 import { Head } from "@/components/seo/head";
+import { ThanksLayout } from "@/features/app/thanks/layouts/thanks-layout";
 import { Send } from "@/features/app/thanks/send/components/send";
-import { useParams } from "react-router-dom";
+
 export const SendRoute = () => {
-  const { userId } = useParams();
   return (
     <>
       <Head title={"送信"} />
-      <ThanksLayout backTo={`/app/${userId}/thanks/location`} label="送信">
+      <ThanksLayout label="送信">
         <Send />
       </ThanksLayout>
     </>
