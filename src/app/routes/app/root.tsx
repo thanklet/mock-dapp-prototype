e3a2f81d9b46ec8bf309ad953159cd98a25ae749
MasatakaItoh <ito@wd-flat.com>
2024-10-02 11:28:04 +0900

    feat: ユーザーアプリ ローディングを画面中央に表示

--- src/app/routes/app/root.tsx
+++ src/app/routes/app/root.tsx
@@ -1,11 +1,24 @@
 import { AppLayout } from "@/features/app/layouts/app-layout";
+import { Box } from "@mui/material";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 export const AppRoot = () => {
   return (
     <AppLayout>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <Box
+            width={"100%"}
+            height={"100vh"}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            Loading...
+          </Box>
+        }
+      >
         <Outlet />
       </Suspense>
     </AppLayout>
