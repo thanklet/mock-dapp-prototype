48e8a7403cf9855615afbd415dba7eb3fa3209ff
Yuki Yamamura <yamamura@wd-flat.com>
2024-11-02 00:29:30 +0900

    feat: プロフィール編集ページを追加する

---
+++ src/features/profile/validation/index.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+const profileSchema = z.object({
+  name: z.string().min(1, { message: "This field is required" }),
+  // NOTE: 編集用のUIがないため、バリデーションは実装しない
+  image_path: z.string(),
+  thanks: z.number(),
+  email: z.string(),
+});
+
+type ProfileSchema = z.infer<typeof profileSchema>;
+
+export { profileSchema, type ProfileSchema };
