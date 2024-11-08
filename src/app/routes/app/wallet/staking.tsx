97c562461922cab365569edceb0c8cc5f978d8be
Yuki Yamamura <yamamura@wd-flat.com>
2024-11-08 21:29:27 +0900

    feat: Wallet > Stakingページを追加する

--- src/app/routes/app/wallet/staking.tsx
+++ src/app/routes/app/wallet/staking.tsx
@@ -1,7 +1,12 @@
+import { Staking } from "@/features/app/wallet/components/staking";
+import { WalletLayout } from "@/features/app/wallet/layouts/wallet-layout";
+
 export const StakingRoute = () => {
   return (
     <div className="flex flex-col gap-y-5 items-center pt-[30px] px-5">
-      <div>staking</div>
+      <WalletLayout label="Staking">
+        <Staking />
+      </WalletLayout>
     </div>
   );
 };
