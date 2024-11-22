1fc3b474114ea27de49d4b11f55a1bf8ede6fc8c
Yuki Yamamura <yamamura@wd-flat.com>
2024-11-22 09:51:24 +0900

    fix: loadStripeをコンポーネントの外側で呼び出す
    https://github.com/thanklet/mock-dapp-prototype/pull/55#discussion_r1851908962

--- src/app/providers/stripe-elements-provider.tsx
+++ src/app/providers/stripe-elements-provider.tsx
@@ -2,10 +2,11 @@ import { usePostStripeIntent } from "@/features/payment/api";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
+const stripePromise = loadStripe("pk_test_46zswMCbz39W2KAqKj43vDRu");
+
 type Props = React.PropsWithChildren;
 
 export const StripeElementsProvider = ({ children }: Props) => {
-  const stripePromise = loadStripe("pk_test_46zswMCbz39W2KAqKj43vDRu");
   const { data: stripeIntent, isStale } = usePostStripeIntent();
 
   // NOTE: Stripe Intentが更新された場合に画面を更新するためのワークアラウンド
