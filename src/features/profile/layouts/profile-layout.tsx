32b639893b189df19a2a4c4831f3f535e67846c2
Yuki Yamamura <yamamura@wd-flat.com>
2024-11-29 19:47:22 +0900

    fix: 全ページでダッシュボードに戻れる様にボタンを追加する

--- src/features/profile/layouts/profile-layout.tsx
+++ src/features/profile/layouts/profile-layout.tsx
@@ -1,19 +1,41 @@
-import { Box } from "@mui/material";
+import { path } from "@/utils/path";
+import { ArrowBackIos } from "@mui/icons-material";
+import { Box, Stack } from "@mui/material";
 import type { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
-type ProfileLayout = {
+type ProfileLayoutProps = {
   children: ReactNode;
 };
 
-export const ProfileLayout = ({ children }: ProfileLayout) => {
+export const ProfileLayout = ({ children }: ProfileLayoutProps) => {
+  const navigate = useNavigate();
+
   return (
     <Box
       sx={{
         maxWidth: "767px",
         margin: "0 auto",
         minHeight: "100vh",
+        paddingTop: "30px",
+        paddingInline: "20px",
       }}
     >
+      <Stack
+        direction="row"
+        p={"10px"}
+        alignItems="center"
+        sx={{
+          width: "100%",
+        }}
+      >
+        <button
+          onClick={() => navigate(path.get().app.dashboard)}
+          type="button"
+        >
+          <ArrowBackIos sx={{ fontSize: "16px" }} />
+        </button>
+      </Stack>
       {children}
     </Box>
   );
